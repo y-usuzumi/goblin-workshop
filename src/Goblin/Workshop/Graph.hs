d9c955a3ff5dd4c742d900cbde5cc86e33f322f4
Yukio Usuzumi <anohigisavay@gmail.com>
2017-06-01 15:38:32 +0800

    initGraph

--- src/Goblin/Workshop/Graph.hs
+++ src/Goblin/Workshop/Graph.hs
@@ -1,7 +1,10 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE ImpredicativeTypes #-}
+
 module Goblin.Workshop.Graph where
 
+import Data.STRef
+import Control.Monad
 import Control.Monad.ST
 import qualified Data.Map as M
 import qualified Data.Vector as V
@@ -23,3 +26,22 @@ connect g@Graph{..} sp ep = let
   edges' = V.modify (\mv -> MV.modify mv (epIdx:) spIdx) edges
   in
   g { edges=edges' }
+
+initGraph :: Ord i
+          => V.Vector (i, a)  -- (key, value) pairs
+          -> V.Vector (i, i)  -- (start, end) unidirections
+          -> Graph i a
+initGraph kvps connections = let
+  vertices = V.map snd kvps
+  iamap = M.fromList $ zip (V.toList $ V.map fst kvps) [0..]
+  edges = runST $ do
+    medges <- MV.replicate (V.length kvps) []
+    flip mapM_ connections $ \(start, end) -> do
+      let startIdx = iamap M.! start
+          endIdx = iamap M.! end
+      MV.modify medges (endIdx:) startIdx
+    V.freeze medges
+  in Graph { vertices
+           , edges
+           , iamap
+           }
